4a4006138807402a096aacdc96b86dbf6ad6af91
Asmairfan202023 <160836108+Asmairfan202023@users.noreply.github.com>
2024-10-01 22:41:39 +0500

    portfolio

---
+++ src/app/components/hero/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="bg-gradient-to-r from-blue-500 flex justify-between items-center mr-4">
+    <h1>My Name is Asma Irfan</h1>
+    <h2>This is My First Assignment in Next.js</h2>
+    <h3>Full stack Web developer</h3>
+    </div>
+  )
+}
+
+export default Hero
